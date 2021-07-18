9de9f9f1be63dfee0c3aa43fa21b2850fa301866
Ruslan <proddost@gmail.com>
2021-07-18 20:23:40 +0300

    new commit in this branch

--- app/src/main/java/com/mycompany/myapplication/workers/LoginFragment.kt
+++ app/src/main/java/com/mycompany/myapplication/workers/LoginFragment.kt
@@ -99,7 +99,7 @@ class LoginFragment : Fragment() {
 
         buttonPassword?.setOnClickListener {
 
-            Log.i("TAG", "${mainViewModel.weatherList.value}")
+            Log.i("TAG", "weatherList ${mainViewModel.weatherList.value}")
 
             changeFragment(SignUpFragment())
         }
